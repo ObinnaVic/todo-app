5f8ed542afb381e5d7c4749c4faf0a53c15d2e0e
Obinna Victor <nkirevictor77@gmail.com>
2022-12-13 08:28:11 +0100

    functionalities added

--- components/Store.js
+++ components/Store.js
@@ -20,6 +20,14 @@ const reducer = (state, action) => {
         const newDoneTask = [...state.doneTasks, item] 
         return{...state, undoneTasks: newUndoneTask, doneTasks: newDoneTask}
     }
+    if (action.type === "REMOVE_UNDONE_ITEMS") {
+        const removedItems = state.undoneTasks.filter((x) => x.id !== action.payload);
+        return {...state, undoneTasks: removedItems};
+    }
+    if (action.type === "REMOVE_DONE_ITEMS") {
+        const removedItems = state.doneTasks.filter((x) => x.id !== action.payload);
+        return {...state, doneTasks: removedItems};
+    }
     return state;
 }
 
--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,19 @@ export default function Home() {
   const HandleTaskDone = (id) => {
     dispatch({type: "TASK_DONE", payload: id})
   }
+
+  const HandleRemoveUndone = (id) => {
+    dispatch({type: "REMOVE_UNDONE_ITEMS", payload: id})
+  }
+
+  const HandleRemove = (id) => {
+    dispatch({ type: "REMOVE_DONE_ITEMS", payload: id });
+  }
   
   return (
     <Layout title={"todo-list"}>
-      <div className="flex justify-center items-center h-screen w-full">
+      <div className="flex flex-col justify-center items-center h-screen w-full bg-cyan-100">
+        <h1 className="text-blue-900 font-bold text-4xl p-2">TO-DO</h1>
         <div className="flex flex-col bg-sky-300 p-10 rounded-lg w-3/4 md:w-1/3">
           <div className="flex">
             <input
@@ -56,16 +65,18 @@ export default function Home() {
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
                       </svg>
                     </button>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="25"
-                      height="25"
-                      fill="currentColor"
-                      className="bi bi-x-circle-fill text-white"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
-                    </svg>
+                    <button onClick={() => HandleRemoveUndone(item.id)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="25"
+                        height="25"
+                        fill="currentColor"
+                        className="bi bi-x-circle-fill text-white"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                      </svg>
+                    </button>
                   </div>
                 </div>
               );
@@ -83,26 +94,18 @@ export default function Home() {
                 <div className="flex justify-between items-center p-3 my-2 bg-slate-400">
                   <p className="text-white">{item.name}</p>
                   <div className="flex">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="25"
-                      height="25"
-                      fill="currentColor"
-                      className="bi bi-check-circle-fill text-white mx-3"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="25"
-                      height="25"
-                      fill="currentColor"
-                      className="bi bi-x-circle-fill text-white"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
-                    </svg>
+                    <button onClick={() => HandleRemove(item.id)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="25"
+                        height="25"
+                        fill="currentColor"
+                        className="bi bi-x-circle-fill text-white"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                      </svg>
+                    </button>
                   </div>
                 </div>
               );
